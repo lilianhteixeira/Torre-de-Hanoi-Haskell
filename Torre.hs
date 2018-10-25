e9ccb210fe64b9066309f3f5bce4c1e620e896a2
bruno <bruno.meneses@ccc.ufcg.edu.br>
2018-10-25 03:06:05 -0300

    Adicionando Modulo Torre com Imprime Torre de Álex Micaela

---
+++ Torre.hs
@@ -0,0 +1,103 @@
+module Torre where
+
+data Torres = Torres {
+    torreA :: [Int],
+    torreB :: [Int],
+    torreC :: [Int],
+    numDiscos :: Int
+} deriving (Show, Read)
+
+insereDisco :: String -> Torres -> Int -> Torres
+insereDisco letra torre disco = atualizaTorre (letra) (torre) (push torreLetra (disco) (topo torreLetra))
+            where torreLetra  = getTorre letra torre
+
+push :: [Int] -> Int -> Int -> [Int]
+push [] _ _= []
+push (x:xs) disco ultimoDisco | x == 0 && ( ultimoDisco == 0 || disco < ultimoDisco) = [disco] ++ xs
+                              | otherwise                     = x : (push xs disco ultimoDisco)
+
+removeDisco :: String -> Torres -> Torres
+removeDisco letra torre = atualizaTorre (letra) (torre) (pop (torreLetra) (topo torreLetra))
+                    where torreLetra = getTorre letra torre
+
+pop :: [Int] -> Int -> [Int]
+pop [] _ = []
+pop (x:xs) ultimoDisco | x /= 0 && x == ultimoDisco = 0 : xs
+                   | otherwise = x : (pop xs ultimoDisco)
+
+getTorre :: String -> Torres -> [Int]
+getTorre letra torres | letra == "a" || letra == "A" = torreA torres
+                      | letra == "b" || letra == "B" = torreB torres
+                      | otherwise                    = torreC torres
+
+tamanhoTorre :: [Int] -> Int
+tamanhoTorre []                 = 0 
+tamanhoTorre (x:xs) | x /= 0    =  1 + tamanhoTorre xs
+                    | otherwise = 0
+
+topo :: [Int] -> Int
+topo torre | indice >= 0 = torre !!indice
+           | otherwise = torre !!0
+            where indice = (tamanhoTorre torre) - 1
+
+estaVazia :: [Int] -> Bool
+estaVazia (x : xs) = x == 0
+estaVazia _ = False
+
+estaCheia :: [Int] -> Int -> Bool
+estaCheia torre numeroDeDiscos = ((tamanhoTorre torre) == numeroDeDiscos)
+
+inicializaTorres :: Int -> Torres
+inicializaTorres numeroDeDiscos= Torres (preencheTorre numeroDeDiscos) (criaTorreVazia numeroDeDiscos) (criaTorreVazia numeroDeDiscos) numeroDeDiscos
+
+preencheTorre :: Int -> [Int]
+preencheTorre numeroDeDiscos = [numeroDeDiscos,(numeroDeDiscos-1)..1] ++ [0]
+
+criaTorreVazia :: Int -> [Int]
+criaTorreVazia numeroDeDiscos = replicate (numeroDeDiscos+1) 0
+
+moveDisco :: String -> String -> Torres -> Torres
+moveDisco origem destino torres = atualizaTorre destino primeiraAtualizacao torreDestino
+                where discoRemovido = topo(getTorre origem torres)
+                      torreOrigem = pop(getTorre origem torres) (discoRemovido)
+                      torreDestino = push(getTorre destino torres) (discoRemovido) (topo(getTorre destino torres))
+                      primeiraAtualizacao = atualizaTorre origem torres torreOrigem
+
+atualizaTorre :: String -> Torres -> [Int] -> Torres
+atualizaTorre letra torres torreAtualizada | letra == "a" || letra == "A" = Torres (torreAtualizada)
+                                                                                   (torreB torres)
+                                                                                   (torreC torres)
+                                                                                   (numDiscos torres)
+
+                                           | letra == "b" || letra == "B" = Torres (torreA torres)
+                                                                                   (torreAtualizada)
+                                                                                   (torreC torres)
+                                                                                   (numDiscos torres)
+
+                                           | otherwise                    = Torres (torreA torres)
+                                                                                   (torreB torres)
+                                                                                   (torreAtualizada)
+                                                                                   (numDiscos torres)
+
+visualizaTorres :: Torres -> IO()
+visualizaTorres torres = constLinhas (numDiscos torres) (torreA torres) (torreB torres) (torreC torres)
+
+
+constLinhas :: Int -> [Int] -> [Int] -> [Int] -> IO()
+constLinhas nDiscos [] [] [] = putStrLn("")
+constLinhas nDiscos [x] [y] [z] = do
+    putStrLn((constColuna nDiscos x) ++ (constColuna nDiscos y) ++ (constColuna nDiscos z))
+constLinhas nDiscos esq meio dir = do
+    putStrLn((constColuna nDiscos (last esq)) ++ (constColuna nDiscos (last meio)) ++ (constColuna nDiscos (last dir)))
+    constLinhas nDiscos (init esq) (init meio) (init dir)
+
+insereQTD :: [Int] -> String -> String
+insereQTD [] y = ""
+insereQTD [x] y = y
+insereQTD (x:xs) y = y ++ insereQTD xs y
+
+constColuna :: Int -> Int -> [Char]
+constColuna nDiscos disco = do
+    if disco /= 0
+    then  insereQTD [0 .. ((nDiscos - disco) - 1)] " " ++ "(" ++ insereQTD [0 .. ((disco * 2) - 1)] "_" ++ ")" ++ insereQTD [0 .. ((nDiscos - disco) - 1)] " "
+    else  insereQTD [0 .. (nDiscos - 1)] " " ++ "||" ++ insereQTD [0 .. (nDiscos - 1)] " "
--- torre-de-hanoi-haskell.hs
+++ torre-de-hanoi-haskell.hs
@@ -1,5 +1,10 @@
 import Data.List
-import System.IO 
+import System.IO
+import System.Process
+import System.Info
+import Torre
+
+clearScreen = if (System.Info.os) == "mingw32" then system "cls" else system "clear"
 
 main :: IO()
 main = do
@@ -19,35 +24,52 @@ inicializar opcao = case opcao of
     4 -> sair
     _ -> main
 
-jogarTorre :: IO()
+jogarTorre :: IO ()
 jogarTorre = do
+    let torres = inicializaTorres 3
+    jogada torres
     putStrLn "fim"
 
+jogada :: Torres -> IO ()
+jogada torres = do
+    clearScreen
+    putStrLn "\tEstado atual"
+    visualizaTorres (torres)
+    putStrLn "\nTorre de Origem: "
+    origem <- getLine
+    putStrLn "\nTorre de Destino: "
+    destino <- getLine
+    jogada (moveDisco origem destino torres)
 
+
+pause :: IO()
+pause = do 
+    putStrLn "Pressione qualquer tecla para continuar..."
+    nada <- getLine
+    putStrLn ""
 
 regras :: IO()
 regras = do
     putStrLn  "\n\n -- Torre de Hanoi: --\n   O objetivo deste jogo, consiste em deslocar todos os discos da primeira haste para a ultima haste.\n   Respeitando as seguintes regras:\n\n     1 - Deslocar um disco de cada vez, o qual devera ser o do topo de uma das tres hastes.\n\n     2 - Cada disco nunca podera ser colocado sobre outro de tamanho menor.\n\n"
     main
 
-
 iaResolve :: IO()
 iaResolve = do
-    putStrLn "\nDigite o numero de discos(Maior ou igual a 3):\n "
-    linha <- getLine
-    let numeroDeDiscos = read linha :: Int
-    avaliandoNumeroDeDiscos . getNumeroDeDiscos $ numeroDeDiscos
-    let esquerda = [1..numeroDeDiscos]
-    let meio = criarListaVazia $  numeroDeDiscos 
-    let direita = criarListaVazia $  numeroDeDiscos
-    putStrLn (constroiTorre esquerda meio direita)
-    let discoRemovido = removeDisco esquerda
-    let opsEsquerda = corrigeHaste esquerda [] []
-    let novoMeio = inserirDisco meio [] discoRemovido  []
-    putStrLn (constroiTorre opsEsquerda novoMeio direita)
-
-    putStrLn "fim"
-
+--     putStrLn "\nDigite o numero de discos(Maior ou igual a 3):\n "
+--     linha <- getLine
+--     let numeroDeDiscos = read linha :: Int
+--     avaliandoNumeroDeDiscos . getNumeroDeDiscos $ numeroDeDiscos
+--     let esquerda = [1..numeroDeDiscos]
+--     let meio = criarListaVazia $  numeroDeDiscos 
+--     let direita = criarListaVazia $  numeroDeDiscos
+--     putStrLn (constroiTorre esquerda meio direita)
+--     let discoRemovido = removeDisco esquerda
+--     let opsEsquerda = corrigeHaste esquerda [] []
+--     let novoMeio = inserirDisco meio [] discoRemovido  []
+--     putStrLn (constroiTorre opsEsquerda novoMeio direita)
+    
+     putStrLn "fim"
+    
 
 sair :: IO()
 sair = do
@@ -72,29 +94,9 @@ erroNumeroDeDiscos = do
 continueNumeroDeDiscos :: IO()
 continueNumeroDeDiscos = do
     putStrLn "\n"   
-
-criarLista :: Int -> [Int]
-criarLista numeroDeDiscos = [1..numeroDeDiscos]
-
-criarListaVazia :: Int -> [Int]
-criarListaVazia numeroDeDiscos = replicate numeroDeDiscos 0
     
 constroiTorre :: [Int] -> [Int] -> [Int] -> String
 constroiTorre esquerda meio direita = (show esquerda) ++ "\n" ++ (show meio) ++ "\n" ++ (show direita) ++ "\n"
-
-inserirDisco :: [Int]-> [Int] -> Int -> [Int] -> [Int] 
-inserirDisco [] (x:xs) elemento discos =  (xs) ++ [elemento] ++ discos
-inserirDisco (x:xs) zeros elemento discos = 
-    if(x == 0)
-        then inserirDisco xs (zeros ++ [x]) elemento discos
-        else inserirDisco xs zeros elemento ([x] ++ discos)
-
-
-removeDisco :: [Int] -> Int
-removeDisco (x:xs) = 
-    if(x /= 0)
-        then x
-        else removeDisco xs
 
 corrigeHaste :: [Int] -> [Int] -> [Int] -> [Int]
 corrigeHaste [] zeros (x:xs) = [0] ++ zeros ++ xs
