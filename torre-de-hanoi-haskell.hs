9b55a113beecddf2900bdba1a9b333c4d8775f42
bruno <bruno.meneses@ccc.ufcg.edu.br>
2018-10-26 17:34:49 -0300

    Adicionando resolução IA e algumas correções na interação com usuário.

--- torre-de-hanoi-haskell.hs
+++ torre-de-hanoi-haskell.hs
@@ -2,6 +2,7 @@ import Data.List
 import System.IO
 import System.Process
 import System.Info
+import Prelude
 import Torre
 
 clearScreen = if (System.Info.os) == "mingw32" then system "cls" else system "clear"
@@ -12,8 +13,17 @@ main = do
     escolha <- getLine
     inicializar $ read escolha
 
+reloadMainWithPause :: IO()
+reloadMainWithPause = do
+    pause
+    clearScreen
+    main
 
 
+reloadMainNoPause :: IO()
+reloadMainNoPause = do
+    clearScreen
+    main
     
 
 inicializar :: Int -> IO()
@@ -22,55 +32,58 @@ inicializar opcao = case opcao of
    2 -> iaResolve
    3 -> regras
    4 -> sair
-   _ -> main
+   _ -> reloadMainNoPause
     
     
 escolherDificuldade :: IO()
 escolherDificuldade = do
-	putStrLn "1 - Fácil \n 2 - Médio \n 3 - Dificil"
-	opc <- getLine
-	let opcao = read opc
-	if opcao == 1 then recebeJogada 3 (inicializaTorres 3)
-	else if opcao == 2 then recebeJogada 4 (inicializaTorres 4)
-	else if opcao == 3 then recebeJogada 5 (inicializaTorres 5)
-	else  putStrLn "Digite um número válido."
-	
-			
+    clearScreen
+    putStrLn "1 - Fácil\n2 - Médio\n3 - Dificil"
+    opc <- getLine
+    let opcao = read opc
+    if opcao == 1 then recebeJogada 3 (inicializaTorres 3)
+    else if opcao == 2 then recebeJogada 4 (inicializaTorres 4)
+    else if opcao == 3 then recebeJogada 5 (inicializaTorres 5)
+    else  putStrLn "Digite um número válido."
+
+vencedor :: IO()
+vencedor = do
+    clearScreen
+    putStrLn "Parabéns você ganhou !!!"
+    putStrLn "\nDeseja jogar novamente?\n1 - Sim\n2 - Não"
+    opc <- getLine
+    let opcao = read opc
+    if (opcao) == 1 then reloadMainNoPause
+    else putStrLn "\n    --- Jogo Finalizado ---"
+
 recebeJogada :: Int -> Torres -> IO()
 recebeJogada n torre = do
-	visualizaTorres torre
-	if (ganhouJogo n torre) == True then putStrLn "Parabéns você ganhou !!!" 
-	else do 
-		putStrLn " Escolha a torre de origem "
-		origem <- getChar
-		putStrLn " Escolha a torre de destino "
-		destino <- getChar
-		if  not(jogadaValida torre n origem destino == True) then jogadaInvalida n torre 
-		else recebeJogada n (moveDisco [origem] [destino] torre)
-	
+    clearScreen
+    visualizaTorres torre
+    if (ganhouJogo n torre) == True then vencedor 
+    else do 
+        putStrLn "\nEscolha a torre de origem (A, B ou C)"
+        origem <- getChar
+        torresSelecionaveis origem
+        destino <- getChar
+        if  not(jogadaValida torre n origem destino == True) then jogadaInvalida n torre 
+        else recebeJogada n (moveDisco [origem] [destino] torre)
+
+torresSelecionaveis :: Char -> IO()
+torresSelecionaveis opc | opc == 'a' || opc == 'A' = putStrLn "\nEscolha a torre de destino (B ou C)"
+                        | opc == 'b' || opc == 'B' = putStrLn "\nEscolha a torre de destino (A ou C)"
+                        | otherwise = putStrLn "\nEscolha a torre de destino (A ou B)"
+
 jogadaInvalida :: Int -> Torres -> IO()
 jogadaInvalida n torre = do 
-	putStrLn "Jogada inválida" 
-	recebeJogada n torre 
+    putStrLn "\nJogada inválida\n" 
+    recebeJogada n torre 
   
 jogadaValida :: Torres -> Int -> Char -> Char -> Bool
 jogadaValida torres n origem destino = topo (getTorre [destino] torres) == 0 || (topo(getTorre [origem]  torres  ) < topo(getTorre [destino] torres))
 
 ganhouJogo :: Int -> Torres -> Bool
 ganhouJogo n torres = (getTorre "C" torres ) == [n,n-1..0]
-		
-
-jogada :: Torres -> IO ()
-jogada torres = do
-    clearScreen
-    putStrLn "\tEstado atual"
-    visualizaTorres (torres)
-    putStrLn "\nTorre de Origem: "
-    origem <- getLine
-    putStrLn "\nTorre de Destino: "
-    destino <- getLine
-    jogada (moveDisco origem destino torres)
-
 
 pause :: IO()
 pause = do 
@@ -81,41 +94,38 @@ pause = do
 regras :: IO()
 regras = do
     putStrLn  "\n\n -- Torre de Hanoi: --\n   O objetivo deste jogo, consiste em deslocar todos os discos da primeira haste para a ultima haste.\n   Respeitando as seguintes regras:\n\n     1 - Deslocar um disco de cada vez, o qual devera ser o do topo de uma das tres hastes.\n\n     2 - Cada disco nunca podera ser colocado sobre outro de tamanho menor.\n\n"
-    main
+    reloadMainWithPause
 
 iaResolve :: IO()
 iaResolve = do
-	putStrLn "\nDigite o numero de discos(Maior ou igual a 3):\n "
-	linha <- getLine
-	let numeroDeDiscos = read linha :: Int
-	avaliandoNumeroDeDiscos . getNumeroDeDiscos $ numeroDeDiscos
-	let torre = inicializaTorres numeroDeDiscos
-	let torreb = jogadaAutomatica numeroDeDiscos torre 'a' 'b' 'c'
-	putStrLn "fim"
-    
-jogadaAutomatica :: Int -> Torres -> Char -> Char -> Char -> Torres
-jogadaAutomatica n torre origem meio destino = do
-	visualizaTorres torre
-	if (ganhouJogo n torre) == True then torre
-	else do 
-		if(n == 1)
-			then casoBase torre origem meio destino
-		else
-		 casoGenerico n torre origem meio destino
-		 
-casoGenerico :: Int -> Torres -> Char -> Char -> Char -> Torres
-casoGenerico n torre origem meio destino = do
-			let torre1 = jogadaAutomatica (n-1) torre origem destino meio
-			let torre2 = moveDisco [origem] [destino] torre1
-			let torre3 = jogadaAutomatica (n-1) torre meio origem destino
-			return torre3
+    putStrLn "\nDigite o numero de discos(Maior ou igual a 3):\n "
+    linha <- getLine
+    let numeroDeDiscos = read linha :: Int
+    avaliandoNumeroDeDiscos . getNumeroDeDiscos $ numeroDeDiscos
+    let torres = inicializaTorres numeroDeDiscos
+    (visualizaIA (torres) ((resolucaoIA (torres) ('a') ('b') ('c') (numeroDeDiscos))))
+    putStrLn "fim"
 
-casoBase :: Torres -> Char -> Char -> Char -> Torres
-casoBase torre origem meio destino = moveDisco [origem] [destino] torre
+resolucaoIA :: Torres -> Char -> Char -> Char -> Int -> [Char]
+resolucaoIA torres origem intermediario destino discos | discos == 1 = [origem] ++ [destino]
+                                                       | otherwise =
+                                                       (resolucaoIA torres origem destino intermediario (discos-1))
+                                                                 ++ [origem] ++ [destino] ++
+                                                       (resolucaoIA torres intermediario origem destino (discos-1))
+
+visualizaIA :: Torres -> [Char] -> IO()
+visualizaIA torres [] = if ((numDiscos torres ) `mod` 2) == 0
+                        then visualizaTorres ((moveDisco (['b']) (['c']) (torres)))
+                        else visualizaTorres ((moveDisco (['a']) (['c']) (torres)))
+visualizaIA torres (x:xs) = do
+    clearScreen
+    visualizaTorres (torres)
+    pause
+    visualizaIA (moveDisco ([x]) ([head xs]) (torres)) (tail xs)
 
 sair :: IO()
 sair = do
-    putStrLn "\n    --- Terminando o Programa ---"
+    putStrLn "\n    --- Jogo Finalizado ---"
 
 
 getNumeroDeDiscos :: Int -> Bool
@@ -136,14 +146,3 @@ erroNumeroDeDiscos = do
 continueNumeroDeDiscos :: IO()
 continueNumeroDeDiscos = do
     putStrLn "\n"   
-    
-constroiTorre :: [Int] -> [Int] -> [Int] -> String
-constroiTorre esquerda meio direita = (show esquerda) ++ "\n" ++ (show meio) ++ "\n" ++ (show direita) ++ "\n"
-
-corrigeHaste :: [Int] -> [Int] -> [Int] -> [Int]
-corrigeHaste [] zeros (x:xs) = [0] ++ zeros ++ xs
-corrigeHaste (x:xs) zeros discos = 
-    if(x == 0)
-        then corrigeHaste xs (zeros ++ [x]) discos
-        else corrigeHaste xs zeros (discos ++ [x])
-        
